d244f1038f961fefd41ed29836e1734323e6e00f
Nicolas Hennick <nicolashennick@gmail.com>
2018-11-10 09:34:14 +0000

    done front-end show and new pages

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,13 +1,18 @@
 function initUpdateNavbarOnScroll() {
+  const banner = document.querySelector('.banner');
   const navbar = document.querySelector('.navbar-wagon');
-  if (navbar) {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-wagon-black');
-      } else {
-        navbar.classList.remove('navbar-wagon-black');
-      }
-    });
+  if (banner) {
+    if (navbar) {
+      window.addEventListener('scroll', () => {
+        if (window.scrollY >= window.innerHeight) {
+          navbar.classList.add('navbar-wagon-black');
+        } else {
+          navbar.classList.remove('navbar-wagon-black');
+        }
+      });
+    }
+  } else {
+    navbar.classList.add('navbar-wagon-black')
   }
 }
 
